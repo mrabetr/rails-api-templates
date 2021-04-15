822e2b01b2bbedd43e23529b62ee3c6471f9137e
rafik mrabet <mrabetr@hotmail.com>
2021-04-15 11:04:39 +0100

    whitelist approach added to secure all controllers with exceptions

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,4 +1,6 @@
-class PublicController < ActionController::API
+class PublicController < ApplicationController
+  skip_before_action :authenticate_request!, only: [:public]
+
   def public
     render json: { message: 'You don\'t need to be authenticated to call this' }
   end
